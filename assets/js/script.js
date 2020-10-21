30bc40f5ee6fd6be39bbcd898a4706c880b19919
Shamakhan786 <67651921+Shamakhan786@users.noreply.github.com>
2020-10-21 11:54:04 -0400

    Dropdown list option

---
+++ assets/js/script.js
@@ -0,0 +1,305 @@
+// initial var
+let input = "";
+
+// Input & Search 1:
+// Click the button to submit a country input & fetch data
+$("#search-buttonC").on("click", function () {
+  document.querySelector("img")?.remove();
+  console.log("country search button is clicked");
+  input = $(this).siblings("#input-div").children("#input").val();
+
+  if (
+    input === "US" ||
+    input === "us" ||
+    input === "usa" ||
+    input === "USA" ||
+    input === "united states" ||
+    input === "america"
+  ) {
+    input = "United States of America";
+  }
+
+  console.log(input);
+  // call the fetch function
+  getCountryData();
+  // clear the input space after each search
+  $(this).siblings("#input-div").children("#input").val("");
+});
+
+// Fetch 1:
+// Get all the available countries & receiving the covid-19 updates
+function getCountryData() {
+  fetch("https://api.covid19api.com/countries")
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(function (responseJson) {
+      // Returns all the available countries, as well as the country slug for per country requests.
+      console.log(responseJson);
+      for (let i = 0; i < responseJson.length; i++) {
+        // Returns the input country
+        if (
+          responseJson[i].Country.includes(input) ||
+          responseJson[i].Slug.includes(input)
+        ) {
+          console.log(responseJson[i]);
+
+          let confirm = document.querySelector("#confirm");
+          let country = document.querySelector("#country-province");
+
+          function getDataByCountry() {
+            fetch(
+              "https://api.covid19api.com/total/dayone/country/" +
+                responseJson[i].Slug
+            )
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (responseJson2) {
+                console.log(responseJson2);
+                let mostRecentIndex = responseJson2.length - 1;
+                console.log(mostRecentIndex);
+                console.log(responseJson2[mostRecentIndex].Active);
+                let mostRecentData = responseJson2[mostRecentIndex].Date.split(
+                  "T"
+                )[0];
+                // print the most recent covid-19 updates data
+                country.textContent = responseJson[i].Country;
+                if (responseJson[i].Country === "United States of America") {
+                  country.textContent = "US";
+                }
+                // let recovered =
+                //   responseJson2[mostRecentIndex].Confirmed -
+                //   responseJson2[mostRecentIndex].Active -
+                //   responseJson2[mostRecentIndex].Deaths;
+                confirm.textContent =
+                  mostRecentData +
+                  ": " +
+                  " Active: " +
+                  responseJson2[mostRecentIndex].Active +
+                  ", " +
+                  " Deaths: " +
+                  responseJson2[mostRecentIndex].Deaths +
+                  ".";
+
+                // create country flag
+                let flagDiv = document.querySelector("#flag");
+                let countryFlag = document.createElement("img");
+
+                countryFlag.setAttribute(
+                  "src",
+                  "https://www.countryflags.io/" +
+                    responseJson[i].ISO2 +
+                    "/flat/64.png"
+                );
+                flagDiv.appendChild(countryFlag);
+              });
+          }
+          // call the data function
+          getDataByCountry();
+        }
+        // if the response doesn't include the user's input country/area, display a message but not an alert
+        else {
+        }
+      }
+    });
+}
+
+// Input & Search 2:
+// Click the button to submit a province input & fetch data
+$("#search-buttonP").on("click", function () {
+  document.querySelector("img")?.remove();
+  console.log("province search button is clicked");
+  input = $(this).siblings("#input-div").children("#input").val();
+
+  console.log(input);
+  // call the fetch function
+  getProvinceData();
+  // clear the input space after each search
+  $(this).siblings("#input-div").children("#input").val("");
+});
+
+// title case function the input for fetch data
+function capitalizeFirstLetter(string) {
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+function titleCase(string) {
+  return string
+    .split(" ")
+    .map((x) => capitalizeFirstLetter(x))
+    .join(" ");
+}
+
+// Fetch 2:
+// Get all the available countries & receiving the covid-19 updates
+function getProvinceData() {
+  fetch("https://www.trackcorona.live/api/provinces")
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(function (responseJson) {
+      // Returns all the available province.
+      console.log(responseJson);
+      for (let i = 0; i < responseJson.data.length; i++) {
+        // Returns the input country
+        if (responseJson.data[i].location.includes(titleCase(input))) {
+          console.log(responseJson.data[i].location);
+          console.log(responseJson.data[i]);
+
+          let confirm = document.querySelector("#confirm");
+          let province = document.querySelector("#country-province");
+          let mostRecentData = responseJson.data[i].updated.split(" ")[0];
+
+          province.textContent =
+            responseJson.data[i].country_code.toUpperCase() +
+            "/" +
+            responseJson.data[i].location;
+
+          confirm.textContent =
+            mostRecentData +
+            ": " +
+            " Confirmed: " +
+            responseJson.data[i].confirmed +
+            ", " +
+            " Deaths: " +
+            responseJson.data[i].dead +
+            ".";
+
+          // create country flag
+          let flagDiv = document.querySelector("#flag");
+          let countryFlag = document.createElement("img");
+
+          countryFlag.setAttribute(
+            "src",
+            "https://www.countryflags.io/" +
+              responseJson.data[i].country_code +
+              "/flat/64.png"
+          );
+          flagDiv.appendChild(countryFlag);
+        }
+      }
+    });
+}
+
+//////////////////////////////// MAP AREA ////////////////////////////////
+am4core.ready(function () {
+  //////////////////////////////// KEEP TOP /////////////////////////////
+  // the world total data
+  // console.log(covid_total_timeline);
+  let lastDateWorld =
+    covid_total_timeline[covid_total_timeline.length - 1].date;
+  let totalConfirmed =
+    covid_total_timeline[covid_total_timeline.length - 1].confirmed;
+  // // print world last updated date and total conformed
+  // console.log(lastDateWorld);
+  // console.log(totalConfirmed);
+
+  // the world country data //
+  // console.log(covid_world_timeline);
+  let mostRecentDate =
+    covid_world_timeline[covid_world_timeline.length - 1].date;
+  console.log("The last update date for all country: " + mostRecentDate);
+  let mostRecentDataCountryAll =
+    covid_world_timeline[covid_world_timeline.length - 1].list;
+  console.log(mostRecentDataCountryAll);
+  //////////////////////////////// KEEP TOP /////////////////////////////
+
+  //////////////////////////////// DATA AREA ////////////////////////////
+  // make a map of country indexes for later use
+  let countryIndexMap = {};
+  // var list = covid_world_timeline[covid_world_timeline.length - 1].list;
+  for (let i = 0; i < mostRecentDataCountryAll.length; i++) {
+    let country = mostRecentDataCountryAll[i];
+    countryIndexMap[country.id] = i;
+  }
+
+  // // calculated active cases in world data (active = confirmed - recovered)
+  // for (let i = 0; i < covid_total_timeline.length; i++) {
+  //   let di = covid_total_timeline[i];
+  //   di.active = di.confirmed - di.recovered;
+  // }
+
+  // function that returns current slide
+  // if index is not set, get last slide
+  function getSlideData(index) {
+    if (index == undefined) {
+      index = covid_world_timeline.length - 1;
+    }
+
+    let data = covid_world_timeline[index];
+
+    return data;
+  }
+
+  // get slide data
+  let slideData = getSlideData();
+
+  // as we will be modifying raw data, make a copy
+  let mapData = JSON.parse(JSON.stringify(slideData.list));
+
+  let max = { confirmed: 0, recovered: 0, deaths: 0 };
+
+  // the last day will have most
+  for (let i = 0; i < mapData.length; i++) {
+    let di = mapData[i];
+    if (di.confirmed > max.confirmed) {
+      max.confirmed = di.confirmed;
+    }
+    if (di.recovered > max.recovered) {
+      max.recovered = di.recovered;
+    }
+    if (di.deaths > max.deaths) {
+      max.deaths = di.deaths;
+    }
+    max.active = max.confirmed;
+  }
+
+  // END OF DATA
+  //////////////////////////////// DATA AREA /////////////////////////////
+
+  //////////////////////////////// MAP SETUP /////////////////////////////
+  // Create map instance
+  let chart = am4core.create("chartdiv", am4maps.MapChart);
+
+  // Set map definition
+  chart.geodata = am4geodata_worldLow;
+
+  // Set projection
+  chart.projection = new am4maps.projections.Miller();
+
+  // Create map polygon series
+  let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+
+  // Make map load polygon (like country names) data from GeoJSON
+  polygonSeries.useGeodata = true;
+  polygonSeries.dataFields.id = "id";
+  polygonSeries.dataFields.value = "confirmed";
+  polygonSeries.data = mapData;
+
+  // Configure series
+  let polygonTemplate = polygonSeries.mapPolygons.template;
+  polygonTemplate.tooltipText =
+    "[bold]{name}:[/]" + "\n" + "[font-size:12px]Total Confirmed: {confirmed}";
+
+  polygonTemplate.fill = am4core.color("#727272");
+
+  // Create hover state and set alternative fill color
+  let hs = polygonTemplate.states.create("hover");
+  hs.properties.fill = am4core.color("#dc3545");
+
+  // Remove Antarctica
+  polygonSeries.exclude = ["AQ"];
+  //////////////////////////////// MAP SETUP /////////////////////////////
+});
+//////////////////////////////// END OF MAP AREA ////////////////////////////////
+
+// for everyday total number display: "https://covid-api.com/api/reports/total"
+
+// // how to capitalize first letter
+// function capitalizeFirstLetter(string) {
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// }
